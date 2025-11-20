dc15cf20a34699f9f3ec9dfd75090a921343dab1
kushagr Sharma <119337482+kushagr1501@users.noreply.github.com>
2025-11-20 09:12:47 +0530

    updated QueueSche,a

--- server/models/Queue.js
+++ server/models/Queue.js
@@ -1,16 +1,36 @@
 import Place from "./Place.js";
 import mongoose from "mongoose";
 
+// const QueueSchema = new mongoose.Schema({
+//   placeId: { type: mongoose.Schema.Types.ObjectId, ref: "Place" },
+//   userName: String,
+//   status: { type: String, default: "waiting" },
+//   joinedAt: { type: Date, default: Date.now },
+
+//   verificationCode: String,                      
+//   isVerified: { type: Boolean, default: false }, 
+//   queueName: { type: String, default: "default" },
+//   verifiedAt: Date                             
+// });
+
+// export default mongoose.model("Queue", QueueSchema);
+
+
 const QueueSchema = new mongoose.Schema({
   placeId: { type: mongoose.Schema.Types.ObjectId, ref: "Place" },
   userName: String,
-  status: { type: String, default: "waiting" },
+  status: { type: String, default: "waiting" }, // waiting / served / cancelled / placeholder
   joinedAt: { type: Date, default: Date.now },
 
-  verificationCode: String,                      
-  isVerified: { type: Boolean, default: false }, 
+  verificationCode: String,
+  isVerified: { type: Boolean, default: false },
   queueName: { type: String, default: "default" },
-  verifiedAt: Date                             
+  verifiedAt: Date,
+
+  // new timestamps
+  servedAt: Date,
+  cancelledAt: Date,
+  acknowledgedAt: Date
 });
 
 export default mongoose.model("Queue", QueueSchema);
